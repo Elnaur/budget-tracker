1ffa0701a1b93cfe38abc5bc774f83749d9952be
Elnaur <nicole.c.loubser@gmail.com>
2024-06-29 00:32:45 +0800

    Added functional tabs and switching between tabs

---
+++ app/src/main/java/com/elnaur/budgettracker/PageAdapter.java
@@ -0,0 +1,37 @@
+package com.elnaur.budgettracker;
+
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+public class PageAdapter extends FragmentPagerAdapter {
+    private Context context;
+    int totalTabs;
+
+    public PageAdapter(Context context, FragmentManager fm, int totalTabs) {
+        super(fm);
+        this.context = context;
+        this.totalTabs = totalTabs;
+    }
+
+    @Override
+    public int getCount() {
+        return totalTabs;
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0:
+                return new AddItemFragment();
+            case 1:
+                return new ViewItemsFragment();
+            default:
+                return null;
+        }
+    }
+}
---
+++ app/src/main/java/com/elnaur/budgettracker/ViewItemsFragment.java
@@ -0,0 +1,22 @@
+package com.elnaur.budgettracker;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class ViewItemsFragment extends Fragment {
+
+    public ViewItemsFragment() {
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_view_items, container, false);
+    }
+}
